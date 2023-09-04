e7e4cd72e55e71bfe57c7ec020e0c623b2d5e735
Zak Grumbles <zakgrumbles@gmail.com>
2023-09-04 16:56:41 -0500

    Nav cards done

--- src/components/NavCard.vue
+++ src/components/NavCard.vue
@@ -1,37 +1,50 @@
 <template>
-    <v-responsive :aspect-ratio="4/3" min-height="150" max-height="350">
-            <v-sheet
-                :elevation="6"
-                rounded
-                class="nav-card-sheet ma-2"
-                height="100%"
+    <v-responsive :aspect-ratio="4 / 3" min-height="150">
+        <v-hover>
+            <div class="nav-card fill-height pa-3">
+            <v-sheet v-bind="props" rounded class="nav-card-sheet fill-height transition-swing" 
                 :style="{ backgroundImage: `url(${background})` }">
                 <div class="fill-height nav-card-body d-flex flex-row justify-center align-center" no-gutters>
-                    <span class="text-h6 nav-card-title">{{ title }}</span>
+                    <span class="text-h6">{{ title }}</span>
                 </div>
             </v-sheet>
+            </div>
+        </v-hover>
     </v-responsive>
 </template>
 
 <style lang="scss" scoped>
 @import "@/styles/settings.scss";
-
 .nav-card-sheet {
-    background-size: cover;
+    position: relative;
     background-position: center;
+    cursor: pointer;
+    box-shadow: 3px 3px 3px rgba(0.0, 0.0, 0.0, 0.3);
+    transition: box-shadow 0.2s ease-in-out;
+    transition: transform 0.1s ease-in-out;
+
+    &:hover {
+        box-shadow: 7px 7px 7px rgba(0.0, 0.0, 0.0, 0.15);
+        transform: scale(1.01);
+    }
 }
 
 .nav-card-body {
+    position: absolute;
     background: rgba(0, 0, 0, 0.3);
     backdrop-filter: blur(2px);
+    top: 0;
+    left: 0;
+    width: 100%;
 }
 
-.nav-card-title {
+span {
     color: $white-warm;
     text-align: center;
     text-justify: center;
     height: min-content;
 }
+
 </style>
 
 <script setup lang="ts">
@@ -40,6 +53,6 @@ interface CardProps {
     background?: string
 }
 
-const props = defineProps<CardProps>()
+defineProps<CardProps>()
 
 </script>
