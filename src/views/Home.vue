10223d238285c111aa6eb3a45b3616a7b54cb1b6
Zak Grumbles <zakgrumbles@protonmail.com>
2024-07-04 12:45:39 -0500

    Blank slate

--- src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Header />
-  <Cards />
-</template>
-
-<script lang="ts" setup>
-  import Header from '@/components/Header.vue'
-  import Cards from '@/components/Cards.vue'
-</script>
