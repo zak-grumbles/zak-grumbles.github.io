d422ec677f65e1513be2a42f30f83165314da842
Zak Grumbles <zakgrumbles@protonmail.com>
2024-07-07 19:23:02 -0500

    Icons created; Componenets started

--- components/DesktopBar.vue
+++ components/DesktopBar.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="absolute left-0 top-0 w-16 h-full bg-slate-900 opacity-95 shadow-lg">
-        <div>
-            <p class="text-slate-100">TESTING ONCE AGAIN</p>
-        </div>
+        <TaskBarItem :icon-path="zg" alt-text="Zak Grumbles" />
+        <TaskBarItem :icon-path="tg" alt-text="terrain-gen" />
     </div>
 </template>
 
 <script setup lang="ts">
+import zg from "~/assets/zg.svg";
+import tg from "~/assets/tg.svg";
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+</style>
---
+++ components/TaskBarItem.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <img :src="props.iconPath"/>
+</div>
+</template>
+
+<script setup lang="ts">
+interface TaskBarProps {
+    iconPath: string,
+    altText: string
+}
+
+const props = defineProps<TaskBarProps>();
+
+</script>
