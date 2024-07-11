6434d4675361a564cb58d5a1cd91ef3173fbdf97
Zak Grumbles <zakgrumbles@protonmail.com>
2024-07-11 11:23:37 -0500

    Finish app window template

--- pages/index.vue
+++ pages/index.vue
@@ -14,25 +14,13 @@
             <AppWindow v-for="proj in projects" class="transition-transform ease-out duration-200" :class="{ 'scale-0': proj.isOpen }">
                 <template v-slot:appbar>
                     <span class="text-white pl-2">{{ proj.title }} Project Info</span>
+                    <span class="text-white float-right pr-2" @click="toggleTGWindow">X</span>
                 </template>
                 <div class="py-2 px-2">
                     <div class="text-2xl text-subtle-gray">{{ proj.title }}</div>
                     <img v-if="proj.screenshot" :src="proj.screenshot" class="pb-2" />
-                    <p>
-                        Terrain-Gen started as the final project for my graphics course,
-                        which was longer ago than I'd like to admit. In a nutshell, the
-                        application uses noise to generate terrain. It was originally written
-                        in immediate mode OpenGL and utilized an un-threaded implementation of
-                        the marching cubes algorithm. It was <span class="italic">slow</span>.
-                    </p>
-                    <br />
-                    <p>
-                        It has recently been re-written in more modern C++ and utilizes the QT Framework
-                        for its user interface. Noise generation is now customizable via a node graph.
-                        It's still a work in progress and there are quite a few features I want to add,
-                        but I'm quite proud of how far it's come.
-                    </p>
-                    <br />
+                    <p v-for="i in proj.description" v-html="i" :key="i"></p>
+                    <br/>
                     <div>
                         <div class="text-xl text-subtle-gray underline">Tech Stack</div>
                         <div class="pt-1">
@@ -53,7 +41,8 @@ interface Project {
     title: string,
     screenshot?: string,
     stack: string[],
-    isOpen: boolean
+    isOpen: boolean,
+    description: string[]
 }
 
 const projects: Ref<Project[]> = ref<Project[]>([
@@ -65,8 +54,19 @@ const projects: Ref<Project[]> = ref<Project[]>([
             'qt-original',
             'opengl-plain'
         ],
-        isOpen: false
-    }
+        isOpen: false,
+        description: [
+            'Terrain-Gen started as the final project for my graphics course, \
+            which was longer ago than I\'d like to admit. In a nutshell, the \
+            application uses noise to generate terrain. It was originally written \
+            in immediate mode OpenGL and utilized an un-threaded implementation of \
+            the marching cubes algorithm. It was <span class="italic">slow</span>.<br/><br/>',
+            'It has recently been re-written in more modern C++ and utilizes the QT Framework \
+            for its user interface. Noise generation is now customizable via a node graph. \
+            It\'s still a work in progress and there are quite a few features I want to add, \
+            but I\'m quite proud of how far it has come.'
+        ]
+    },
 ])
 
 const isHidden: Ref<boolean> = ref<boolean>(true);
